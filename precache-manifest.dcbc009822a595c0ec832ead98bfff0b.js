721494fbf058faacc99bb47886878579552b8b15
Mukilan A <Mukilan_A@external.mckinsey.com>
2020-10-28 11:59:22 +0530

    Updates

---
+++ precache-manifest.dcbc009822a595c0ec832ead98bfff0b.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b94e5b29af33f79e268776e534d0e54",
+    "url": "/deploy-alpha/index.html"
+  },
+  {
+    "revision": "cab4db271a8cc15964a6",
+    "url": "/deploy-alpha/static/css/2.8128b923.chunk.css"
+  },
+  {
+    "revision": "ad3e8ef1e2aa4198c417",
+    "url": "/deploy-alpha/static/css/main.c5af5479.chunk.css"
+  },
+  {
+    "revision": "cab4db271a8cc15964a6",
+    "url": "/deploy-alpha/static/js/2.5218b6a3.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/deploy-alpha/static/js/2.5218b6a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad3e8ef1e2aa4198c417",
+    "url": "/deploy-alpha/static/js/main.37d24954.chunk.js"
+  },
+  {
+    "revision": "ecc856e55f75478ffdbf",
+    "url": "/deploy-alpha/static/js/runtime-main.5db73b08.js"
+  },
+  {
+    "revision": "f8b1df51ba843179fa1cc9b53d58127a",
+    "url": "/deploy-alpha/static/media/roboto-latin-100-italic.f8b1df51.woff2"
+  },
+  {
+    "revision": "f9e8e590b4e0f1ff83469bb2a55b8488",
+    "url": "/deploy-alpha/static/media/roboto-latin-100-italic.f9e8e590.woff"
+  },
+  {
+    "revision": "5cb7edfceb233100075dc9a1e12e8da3",
+    "url": "/deploy-alpha/static/media/roboto-latin-100-normal.5cb7edfc.woff"
+  },
+  {
+    "revision": "7370c3679472e9560965ff48a4399d0b",
+    "url": "/deploy-alpha/static/media/roboto-latin-100-normal.7370c367.woff2"
+  },
+  {
+    "revision": "14286f3ba79c6627433572dfa925202e",
+    "url": "/deploy-alpha/static/media/roboto-latin-300-italic.14286f3b.woff2"
+  },
+  {
+    "revision": "4df32891a5f2f98a363314f595482e08",
+    "url": "/deploy-alpha/static/media/roboto-latin-300-italic.4df32891.woff"
+  },
+  {
+    "revision": "b00849e00f4c2331cddd8ffb44a6720b",
+    "url": "/deploy-alpha/static/media/roboto-latin-300-normal.b00849e0.woff"
+  },
+  {
+    "revision": "ef7c6637c68f269a882e73bcb57a7f6a",
+    "url": "/deploy-alpha/static/media/roboto-latin-300-normal.ef7c6637.woff2"
+  },
+  {
+    "revision": "51521a2a8da71e50d871ac6fd2187e87",
+    "url": "/deploy-alpha/static/media/roboto-latin-400-italic.51521a2a.woff2"
+  },
+  {
+    "revision": "fe65b8335ee19dd944289f9ed3178c78",
+    "url": "/deploy-alpha/static/media/roboto-latin-400-italic.fe65b833.woff"
+  },
+  {
+    "revision": "479970ffb74f2117317f9d24d9e317fe",
+    "url": "/deploy-alpha/static/media/roboto-latin-400-normal.479970ff.woff2"
+  },
+  {
+    "revision": "60fa3c0614b8fb2f394fa29944c21540",
+    "url": "/deploy-alpha/static/media/roboto-latin-400-normal.60fa3c06.woff"
+  },
+  {
+    "revision": "288ad9c6e8b43cf02443a1f499bdf67e",
+    "url": "/deploy-alpha/static/media/roboto-latin-500-italic.288ad9c6.woff"
+  },
+  {
+    "revision": "db4a2a231f52e497c0191e8966b0ee58",
+    "url": "/deploy-alpha/static/media/roboto-latin-500-italic.db4a2a23.woff2"
+  },
+  {
+    "revision": "020c97dc8e0463259c2f9df929bb0c69",
+    "url": "/deploy-alpha/static/media/roboto-latin-500-normal.020c97dc.woff2"
+  },
+  {
+    "revision": "87284894879f5b1c229cb49c8ff6decc",
+    "url": "/deploy-alpha/static/media/roboto-latin-500-normal.87284894.woff"
+  },
+  {
+    "revision": "81f57861ed4ac74741f5671e1dff2fd9",
+    "url": "/deploy-alpha/static/media/roboto-latin-700-italic.81f57861.woff"
+  },
+  {
+    "revision": "da0e717829e033a69dec97f1e155ae42",
+    "url": "/deploy-alpha/static/media/roboto-latin-700-italic.da0e7178.woff2"
+  },
+  {
+    "revision": "2735a3a69b509faf3577afd25bdf552e",
+    "url": "/deploy-alpha/static/media/roboto-latin-700-normal.2735a3a6.woff2"
+  },
+  {
+    "revision": "adcde98f1d584de52060ad7b16373da3",
+    "url": "/deploy-alpha/static/media/roboto-latin-700-normal.adcde98f.woff"
+  },
+  {
+    "revision": "28f9151055c950874d2c6803a39b425b",
+    "url": "/deploy-alpha/static/media/roboto-latin-900-italic.28f91510.woff"
+  },
+  {
+    "revision": "ebf6d1640ccddb99fb49f73c052c55a8",
+    "url": "/deploy-alpha/static/media/roboto-latin-900-italic.ebf6d164.woff2"
+  },
+  {
+    "revision": "9b3766ef4a402ad3fdeef7501a456512",
+    "url": "/deploy-alpha/static/media/roboto-latin-900-normal.9b3766ef.woff2"
+  },
+  {
+    "revision": "bb1e4dc6333675d11ada2e857e7f95d7",
+    "url": "/deploy-alpha/static/media/roboto-latin-900-normal.bb1e4dc6.woff"
+  }
+]);
